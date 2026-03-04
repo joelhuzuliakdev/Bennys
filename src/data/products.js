74f2b41d43202830a416cdfef4620b13783ac95f
joelhuzuliakdev <joelhuzuliakdev@outlook.com>
2026-03-04 18:50:25 -0300

    agregue productos y cambie horarios

--- src/data/products.js
+++ src/data/products.js
@@ -4,8 +4,8 @@ export const products = [
     name: "Cuarto Benny´s",
     category: "hamburguesas",
     price: 12000,
-    doble: 13000,
-    triple: 14000,
+    doble: 15000,
+    triple: 17900,
     image: "/hamburguesas/hamburguesa-simple.jpg",
     description: "Pan de papa, carne, cebolla brunoise, mostaza y kétchup",
     ingredients: [
@@ -19,8 +19,8 @@ export const products = [
     name: "Big American",
     category: "hamburguesas",
     price: 12000,
-    doble: 13000,
-    triple: 14000,
+    doble: 15000,
+    triple: 17900,
     image: "/hamburguesas/hamburguesa-doble.jpg",
     description: "Pan de papa, carne, queso cheeddar, tomate, lechuga, cebolla brunoise y mayonesa",
     ingredients: [
@@ -36,8 +36,8 @@ export const products = [
     name: "Cheese Burguer",
     category: "hamburguesas",
     price: 12000,
-    doble: 13000,
-    triple: 14000,
+    doble: 15000,
+    triple: 17900,
     image: "/hamburguesas/hamburguesa-triple.jpg",
     description: "Pan de papa, carne, queso cheeddar",
     ingredients: [
@@ -49,8 +49,8 @@ export const products = [
     name: "Benny´s Melt",
     category: "hamburguesas",
     price: 12000,
-    doble: 13000,
-    triple: 14000,
+    doble: 15000,
+    triple: 17900,
     image: "/hamburguesas/hamburguesa-simple.jpg",
     description: "Pan de papa, carne, triple cheddar bien derretido, cebolla caramelizada y aderezo Benny´s",
     ingredients: [
@@ -64,8 +64,8 @@ export const products = [
     name: "Benny´s Deluxe",
     category: "hamburguesas",
     price: 12000,
-    doble: 13000,
-    triple: 14000,
+    doble: 15000,
+    triple: 17900,
     image: "/hamburguesas/hamburguesa-simple.jpg",
     description: "Pan de papa, carne, queso cheddar blanco ahumado, panceta, lechuga, tomate y aderezo Benny´s",
     ingredients: [
@@ -81,8 +81,8 @@ export const products = [
     name: "Benny´s Crunch",
     category: "hamburguesas",
     price: 12000,
-    doble: 13000,
-    triple: 14000,
+    doble: 15000,
+    triple: 17900,
     image: "/hamburguesas/hamburguesa-simple.jpg",
     description: "Pan de papa, carne, queso cheddar, panceta, cebolla crispy y kétchup",
     ingredients: [
@@ -97,8 +97,8 @@ export const products = [
     name: "Melt Turbo",
     category: "hamburguesas",
     price: 12000,
-    doble: 13000,
-    triple: 14000,
+    doble: 15000,
+    triple: 17900,
     image: "/hamburguesas/hamburguesa-simple.jpg",
     description: "Pan tostado doble, carne, cebolla caramelizada, cebolla crispy, panceta, kétchup, mayonesa y triple cheddar",
     ingredients: [
@@ -132,8 +132,8 @@ export const products = [
     name: "Benny´s Veggie",
     category: "vegano",
     price: 12000,
-    doble: 13000,
-    triple: 14000,
+    doble: 15000,
+    triple: 17900,
     image: "/vegano/hamburguesa-vegana.jpg",
     description: "Pan de papa, medallón VEGGIE, queso cheddar y toppings a elección",
     ingredients: [
@@ -143,7 +143,179 @@ export const products = [
   },
   {
     id: 10,
-    name: "Coca Cola",
+    name: "Lomo Benny´s",
+    category: "lomitos",
+    price: 18000,
+    image: "/vegano/hamburguesa-vegana.jpg",
+    description: "Pan, aderezo Benny´s, panceta, cebolla rehogada, queso cheddar, carne 100% lomo",
+    ingredients: [
+      "Aderezo Benny´s",
+      "Panceta",
+      "Cebolla Rehogada",
+      "Queso Cheddar",
+      "Carne Lomo"
+    ]
+  },
+  {
+    id: 11,
+    name: "Lomo Tradicional",
+    category: "lomitos",
+    price: 18000,
+    image: "/vegano/hamburguesa-vegana.jpg",
+    description: "Pan, mayo casera, tomate, lechuga, queso tybo, jamón, huevo, carne 100% lomo",
+    ingredients: [
+      "Mayo Casera",
+      "Tomate",
+      "Lechuga",
+      "Queso Tybo",
+      "Jamón",
+      "Huevo",
+      "Carne Lomo"
+    ]
+  },
+  {
+    id: 12,
+    name: "Sandwich Philly Cheese Benny´s",
+    category: "sandwiches",
+    price: 15000,
+    image: "/vegano/hamburguesa-vegana.jpg",
+    description: "Pan tostado, carne y cebolla salteado al estilo americano, panceta crocante, cheddar ahumado fundido",
+    ingredients: [
+      "Pan Tostado",
+      "Carne y Cebolla Salteado",
+      "Panceta Crocante",
+      "Cheddar Ahumado Fundido"
+    ]
+  },
+  {
+    id: 13,
+    name: "Sandwich Philly Cheese Tradicional",
+    category: "sandwiches",
+    price: 15000,
+    image: "/vegano/hamburguesa-vegana.jpg",
+    description: "Pan tostado, carne y cebolla salteado al estilo americano, queso cheddar fundido",
+    ingredients: [
+      "Pan Tostado",
+      "Carne y Cebolla Salteado",
+      "Cheddar Fundido"
+    ]
+  },
+  {
+    id: 14,
+    name: "Sandwich Philly Cheese Morron",
+    category: "sandwiches",
+    price: 15000,
+    image: "/vegano/hamburguesa-vegana.jpg",
+    description: "Pan tostado, carne y cebolla salteado al estilo americano, pimiento salteados, queso cheddar fundido",
+    ingredients: [
+      "Pan Tostado",
+      "Carne y Cebolla Salteado",
+      "Pimiento Salteado",
+      "Cheddar Fundido"
+    ]
+  },
+  {
+    id: 15,
+    name: "Sandwich Meatball",
+    category: "sandwiches",
+    price: 0,
+    image: "/vegano/hamburguesa-vegana.jpg",
+    description: "Pan tostado, albondigas caseras en salsa roja, cubiertas con mozzarella fundida, lluvia de parmesano",
+    ingredients: [
+      "Pan Tostado",
+      "Albondigas Caseras En Salsa Roja",
+      "Lluvia De Parmesano"
+    ]
+  },
+  {
+    id: 16,
+    name: "Papas Benny´s",
+    category: "snacks",
+    price: 13000,
+    image: "/gaseosas/coca-cola.jpg",
+    description: "Porcion de papas fritas XL, queso cheddar fundido, trozitos de carne smash, aderezo BENNY´S",
+    ingredients: [
+      "Papas Fritas",
+      "Queso Cheddar Fundido",
+      "Trozitos de Carne Smash",
+      "Aderezo Benny´s"
+    ]
+  },
+  {
+    id: 17,
+    name: "Tradicionales",
+    category: "snacks",
+    price: 9000,
+    image: "/gaseosas/coca-cola.jpg",
+    description: "Porcion de papas fritas XL tradicionales con aderezo a elección",
+    ingredients: [
+      "Papas Fritas",
+      "Aderezo"
+    ]
+  },
+  {
+    id: 18,
+    name: "Americanas",
+    category: "snacks",
+    price: 11000,
+    image: "/gaseosas/coca-cola.jpg",
+    description: "Porcion de papas fritas XL, queso cheddar fundido, panceta y cebolla de verdeo",
+    ingredients: [
+      "Papas Fritas",
+      "Queso Cheddar Fundido",
+      "Panceta y Cebolla De Verdeo"
+    ]
+  },
+  {
+    id: 19,
+    name: "Bastones de Mozzarella",
+    category: "snacks",
+    price: 11000,
+    image: "/gaseosas/coca-cola.jpg",
+    description: "Porción de bastoncitos de mozzarella fritos, con aderezo a elección",
+    ingredients: [
+      "Bastones de Mozzarella Fritos",
+      "Aderezo"
+    ]
+  },
+  {
+    id: 20,
+    name: "Picadita Benny´s",
+    category: "snacks",
+    price: 16000,
+    image: "/gaseosas/coca-cola.jpg",
+    description: "Salame, jamón crudo, jamón cocido, mix de quesos, papitas fritas, maní, aceitunas, pan casero, (PARA 2 PERSONAS)",
+    ingredients: [
+      "Salame",
+      "Jamón Crudo",
+      "Jamón Cocido",
+      "Mix de Quesos",
+      "Papitas Fritas",
+      "Maní",
+      "Aceitunas",
+      "Pan Casero"
+    ]
+  },
+  {
+    id: 21,
+    name: "Picadita Benny´s 2.0",
+    category: "snacks",
+    price: 0,
+    image: "/gaseosas/coca-cola.jpg",
+    description: "Papas fritas con cheddar y bacon, bastones de mozzarella, nugets de pollo frito, salchichitas con aderezo, aceitunas, maní, pan casero",
+    ingredients: [
+      "Papitas Fritas Con Cheddar y Bacon",
+      "Bastones de Mozzarella",
+      "Nugets de Pollo Frito",
+      "Salchicas Con Aderezo",
+      "Aceitunas",
+      "Maní",
+      "Pan Casero"
+    ]
+  },
+  {
+    id: 22,
+    name: "Pepsi",
     category: "bebidas",
     price: 3000,
     image: "/gaseosas/coca-cola.jpg",
@@ -151,8 +323,8 @@ export const products = [
     ingredients: []
   },
   {
-    id: 11,
-    name: "Sprite",
+    id: 23,
+    name: "Mirinda",
     category: "bebidas",
     price: 3000,
     image: "/gaseosas/sprite.jpg",
@@ -160,8 +332,8 @@ export const products = [
     ingredients: []
   },
   {
-    id: 12,
-    name: "Pepsi",
+    id: 24,
+    name: "Seven Up",
     category: "bebidas",
     price: 3000,
     image: "/gaseosas/pepsi.jpg",
@@ -169,7 +341,115 @@ export const products = [
     ingredients: []
   },
   {
-    id: 13,
+    id: 25,
+    name: "Paso De Los Toros",
+    category: "bebidas",
+    price: 3000,
+    image: "/gaseosas/pepsi.jpg",
+    description: "Bebida refrescante 500ml",
+    ingredients: []
+  },
+  {
+    id: 26,
+    name: "Pepsi Zero Black",
+    category: "bebidas",
+    price: 3000,
+    image: "/gaseosas/pepsi.jpg",
+    description: "Bebida refrescante 500ml",
+    ingredients: []
+  },
+  {
+    id: 27,
+    name: "Seven Up Zero",
+    category: "bebidas",
+    price: 3000,
+    image: "/gaseosas/pepsi.jpg",
+    description: "Bebida refrescante 500ml",
+    ingredients: []
+  },
+  {
+    id: 28,
+    name: "Agua Mineral",
+    category: "bebidas",
+    price: 3000,
+    image: "/gaseosas/pepsi.jpg",
+    description: "Bebida refrescante 500ml",
+    ingredients: []
+  },
+  {
+    id: 29,
+    name: "Red Bull",
+    category: "bebidas",
+    price: 3000,
+    image: "/gaseosas/pepsi.jpg",
+    description: "Bebida refrescante 500ml",
+    ingredients: []
+  },
+  {
+    id: 30,
+    name: "Stella Artois",
+    category: "bebidas",
+    price: 9000,
+    image: "/gaseosas/pepsi.jpg",
+    description: "Bebida refrescante 975cc",
+    ingredients: []
+  },
+  {
+    id: 31,
+    name: "Brahma Chopp",
+    category: "bebidas",
+    price: 7000,
+    image: "/gaseosas/pepsi.jpg",
+    description: "Bebida refrescante 975cc",
+    ingredients: []
+  },
+  {
+    id: 32,
+    name: "Andes Ipa",
+    category: "bebidas",
+    price: 7000,
+    image: "/gaseosas/pepsi.jpg",
+    description: "Bebida refrescante 975cc",
+    ingredients: []
+  },
+  {
+    id: 33,
+    name: "Andes Rubia",
+    category: "bebidas",
+    price: 7000,
+    image: "/gaseosas/pepsi.jpg",
+    description: "Bebida refrescante 975cc",
+    ingredients: []
+  },
+  {
+    id: 34,
+    name: "Andes Roja",
+    category: "bebidas",
+    price: 7000,
+    image: "/gaseosas/pepsi.jpg",
+    description: "Bebida refrescante 975cc",
+    ingredients: []
+  },
+  {
+    id: 35,
+    name: "Corona",
+    category: "bebidas",
+    price: 5000,
+    image: "/gaseosas/pepsi.jpg",
+    description: "Bebida refrescante 355cc",
+    ingredients: []
+  },
+  {
+    id: 36,
+    name: "Budweiser",
+    category: "bebidas",
+    price: 0,
+    image: "/gaseosas/pepsi.jpg",
+    description: "Bebida refrescante 355cc",
+    ingredients: []
+  },
+  {
+    id: 37,
     name: "Chocotorta",
     category: "postres",
     price: 8000,
@@ -178,7 +458,7 @@ export const products = [
     ingredients: []
   },
   {
-    id: 14,
+    id: 38,
     name: "Postre Borracho",
     category: "postres",
     price: 8000,
@@ -187,7 +467,7 @@ export const products = [
     ingredients: []
   },
   {
-    id: 15,
+    id: 39,
     name: "Ensalada de Frutas",
     category: "postres",
     price: 8000,
@@ -200,6 +480,9 @@ export const products = [
 export const categories = [
   { id: "todos", name: "Todos" },
   { id: "hamburguesas", name: "Hamburguesas" },
+  { id: "lomitos", name: "Lomitos" },
+  { id: "sandwiches", name: "Sandwiches" },
+  { id: "snacks", name: "Snacks" },
   { id: "vegano", name: "Vegano" },
   { id: "postres", name: "Postres" },
   { id: "bebidas", name: "Bebidas" }
